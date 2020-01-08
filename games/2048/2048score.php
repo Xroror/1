82b40f35d9ab882f3abb949535176c8367a3442e
Xroror <59664227+Xroror@users.noreply.github.com>
2020-01-08 21:35:30 +0200

    Create 2048score.php

---
+++ games/2048/2048score.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+$score = $_POST["score"];
+$id = $_SESSION["login_id"];
+
+$servername = "localhost";
+$dbname = "auch";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($servername, $username, $password, $dbname); # establish a connection
+if($conn->connect_error){
+	die("Connection failed: " . $conn->connection_error);
+}
+$sqlq = "SELECT score_value FROM 2048ld WHERE acc_id = '$id'";
+$result = $conn->query($sqlq);
+if($result->num_rows > 0){
+	$row = $result->fetch_assoc();
+	if ($score > $row['score_value']){
+		$sqlq = "INSERT INTO 2048ld (acc_id, score_value) VALUES ('$id', '$score')
+		ON DUPLICATE KEY UPDATE score_value = $score";
+		$result = $conn->query($sqlq);
+	}else if ($score < $row['score_value']){
+		
+	}
+}else if($result->num_rows == 0){
+	$sqlq = "INSERT INTO 2048ld (acc_id, score_value) VALUES ('$id', '$score')";
+		$result = $conn->query($sqlq);
+	}
+	
+
+var_dump($result);
+?>
+
+
